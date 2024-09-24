7f0816839ebe111972342a3a09d814482523034a
Nath Caragao <nboregas@gmail.com>
2024-09-24 07:04:40 +0000

    Added some comments in main.ts file for better understanding

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+// This is the entry point fuunction for Nakama Server
 let InitModule: nkruntime.InitModule = function (
+  // Context is said to be info about server like env variables (haven't grasped yet)
   ctx: nkruntime.Context,
+  // Logger manages server logs
   logger: nkruntime.Logger,
+  // NK is responsible for being the middleware to call server-side functions
   nk: nkruntime.Nakama,
+  // Initializer is said to register PC's hook and callbacks (haven't grasped yet)
   initializer: nkruntime.Initializer
 ) {
   logger.info("Hello World!");
