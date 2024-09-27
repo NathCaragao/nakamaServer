1c932d8f0036c87226e105e96a19098a259270f9
Nath Caragao <nboregas@gmail.com>
2024-09-27 04:56:07 +0000

    compiled codes

--- build/index.js
+++ build/index.js
@@ -9,41 +9,86 @@ logger,
 nk, 
 // Initializer is said to register RPCs hook and callbacks (haven't grasped yet)
 initializer) {
+    initializer.registerMatch("testMatchHandler", {
+        matchInit: matchInit1,
+        matchJoinAttempt: matchJoinAttempt1,
+        matchJoin: matchJoin1,
+        matchLoop: matchLoop1,
+        matchLeave: matchLeave1,
+        matchTerminate: matchTerminate1,
+        matchSignal: matchSignal1,
+    });
+    registerRPCs(initializer);
     logger.info("Hello World!");
 };
-var matchInit = function (ctx, logger, nk, params) {
+var registerRPCs = function (initializer) {
+    initializer.registerRpc("createMatchRPC", createMatchRPC);
+};
+var matchInit1 = function (ctx, logger, nk, params) {
+    logger.debug('Lobby match created');
+    var presences = {};
     return {
-        state: { presences: {}, emptyTicks: 0 },
-        tickRate: 1, // 1 tick per second = 1 MatchLoop func invocations per second
-        label: "",
+        state: { presences: presences },
+        tickRate: 1,
+        label: ''
     };
 };
-var matchJoin = function (ctx, logger, nk, dispatcher, tick, state, presences) {
-    presences.forEach(function (p) {
-        state.presences[p.sessionId] = p;
+var matchJoinAttempt1 = function (ctx, logger, nk, dispatcher, tick, state, presence, metadata) {
+    logger.debug('%q attempted to join Lobby match', ctx.userId);
+    return {
+        state: state,
+        accept: true
+    };
+};
+var matchJoin1 = function (ctx, logger, nk, dispatcher, tick, state, presences) {
+    presences.forEach(function (presence) {
+        state.presences[presence.userId] = presence;
+        logger.debug('%q joined Lobby match', presence.userId);
     });
     return {
-        state: state,
+        state: state
     };
 };
-var matchLeave = function (ctx, logger, nk, dispatcher, tick, state, presences) {
-    presences.forEach(function (p) {
-        delete state.presences[p.sessionId];
+var matchLeave1 = function (ctx, logger, nk, dispatcher, tick, state, presences) {
+    presences.forEach(function (presence) {
+        delete (state.presences[presence.userId]);
+        logger.debug('%q left Lobby match', presence.userId);
     });
     return {
-        state: state,
+        state: state
     };
 };
-var matchLoop = function (ctx, logger, nk, dispatcher, tick, state, messages) {
-    // If we have no presences in the match according to the match state, increment the empty ticks count
-    if (state.presences.length === 0) {
-        state.emptyTicks++;
-    }
-    // If the match has been empty for more than 100 ticks, end the match by returning null
-    if (state.emptyTicks > 100) {
-        return null;
-    }
+var matchLoop1 = function (ctx, logger, nk, dispatcher, tick, state, messages) {
+    logger.debug('Lobby match loop executed');
+    Object.keys(state.presences).forEach(function (key) {
+        var presence = state.presences[key];
+        logger.info('Presence %v name $v', presence.userId, presence.username);
+    });
+    messages.forEach(function (message) {
+        logger.info('Received %v from %v', message.data, message.sender.userId);
+        dispatcher.broadcastMessage(1, message.data, [message.sender], null);
+    });
+    return {
+        state: state
+    };
+};
+var matchTerminate1 = function (ctx, logger, nk, dispatcher, tick, state, graceSeconds) {
+    logger.debug('Lobby match terminated');
+    var message = "Server shutting down in ".concat(graceSeconds, " seconds.");
+    dispatcher.broadcastMessage(2, message, null, null);
+    return {
+        state: state
+    };
+};
+var matchSignal1 = function (ctx, logger, nk, dispatcher, tick, state, data) {
+    logger.debug('Lobby match signal received: ' + data);
     return {
         state: state,
+        data: "Lobby match signal received: " + data
     };
 };
+function createMatchRPC(context, logger, nk, payload) {
+    logger.debug(payload);
+    var matchId = nk.matchCreate("testMatchHandler");
+    return JSON.stringify({ matchId: matchId });
+}
