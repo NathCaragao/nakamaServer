f856db6a33e8824de200910f4328c9466403c5ac
NathCaragao <166087791+NathCaragao@users.noreply.github.com>
2024-12-08 05:42:45 +0800

    played around some more in landing page

--- adminFrontend/theous_kai_frontend/src/LandingPage/LandingPage.jsx
+++ adminFrontend/theous_kai_frontend/src/LandingPage/LandingPage.jsx
@@ -4,7 +4,7 @@ import "./LandingPage.css";
 const LandingPage = () => {
   return (
     <>
-        <div className='container-fluid main-div p-0 m-0 d-flex flex-column'>
+        <div className='container-fluid main-div p-0 m-0 d-flex flex-column bg-secondary'>
             <div className="row align-items-center justify-content-center h-75 p-0 m-0 flex-grow-1">
                 <div className="column hero-content w-50 align-items-center justify-content-center text-center p-0 m-0">
                     <h1 className='m-5'>THEOUS KAI</h1>
@@ -12,7 +12,7 @@ const LandingPage = () => {
                     <button className='m-5 btn btn-primary'>Download</button>
                 </div>
             </div>
-            <div className="row align-items-center justify-content-center h-auto p-0 m-0 flex-shrink-0 text-end">
+            <div className="row align-items-center justify-content-center h-auto p-0 m-0 flex-shrink-0 text-end text-white">
                 <p>© 2024-present. All rights reserved. NCST S.Y. 2024-2025 PCJS-001 BSCS41-E1 Group 1</p>
             </div>
         </div>
