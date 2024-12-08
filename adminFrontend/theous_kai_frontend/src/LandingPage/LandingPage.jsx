8c7baaccc0a4da88ff2dc37880ae582f1509ac17
NathCaragao <166087791+NathCaragao@users.noreply.github.com>
2024-12-09 00:24:56 +0800

    Changed Landing Page elements

--- adminFrontend/theous_kai_frontend/src/LandingPage/LandingPage.jsx
+++ adminFrontend/theous_kai_frontend/src/LandingPage/LandingPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import "./LandingPage.css";
+import windowsLogo from "../../assets/windows-logo.png";
+
+const downloadGame = () => {
+    
+}
 
 const LandingPage = () => {
   return (
@@ -8,12 +13,17 @@ const LandingPage = () => {
             <div className="row align-items-center justify-content-center h-75 p-0 m-0 flex-grow-1">
                 <div className="column hero-content w-50 align-items-center justify-content-center text-center p-0 m-0">
                     <h1 className='m-5'>THEOUS KAI</h1>
-                    <h5 className='m-5'>"Learn about bullshit here!"</h5>
-                    <button className='m-5 btn btn-primary'>Download</button>
+                    <h5 className='my-3 mx-5 hero-text'>
+                        Have you ever wanted to dive into the fascinating world of Greek Mythology but found massive texts
+                        overwhelming and boring? Fear not! Theous Kai is here to help. Explore the wonders
+                        of Greek mythology through an exciting platformer experience that turns learning into an adventure.
+                        Forget the boredom as you jump, run, and immerse yourself in the stories of Greek Mythology!
+                    </h5>
+                    <button className='m-3 btn btn-primary'>Download Now <img src={windowsLogo} alt="Windows Device only" className='mx-3'/></button>
                 </div>
             </div>
             <div className="row align-items-center justify-content-center h-auto p-0 m-0 flex-shrink-0 text-end text-white">
-                <p>© 2024-present. All rights reserved. NCST S.Y. 2024-2025 PCJS-001 BSCS41-E1 Group 1</p>
+                <p>© 2024-present. All rights reserved. NCST S.Y. 2024-2025 PCJS-002 BSCS41-E1 Group 1</p>
             </div>
         </div>
     </>
