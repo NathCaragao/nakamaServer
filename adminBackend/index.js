c49e9ec112b55eaf49ebd1c287f42ab351cc16bb
NathCaragao <166087791+NathCaragao@users.noreply.github.com>
2024-12-08 10:39:15 +0800

    Fixed backend code to run and access the server

--- adminBackend/index.js
+++ adminBackend/index.js
@@ -7,9 +7,13 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT;
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Nakama is at ${process.env.NAKAMA_CONSOLE_ADDRESS}`);
 });
 
-
+app.get("/", async(request, response) => {
+    return response.status(200).json({
+        message: "Great!",
+    })
+});
 
