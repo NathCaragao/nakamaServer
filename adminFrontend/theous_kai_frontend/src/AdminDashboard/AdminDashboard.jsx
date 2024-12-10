7745f99c35b48f2d57c9f3ed5c4e2548b99f9285
NathCaragao <166087791+NathCaragao@users.noreply.github.com>
2024-12-10 18:42:35 +0800

    Progress in admin dashboard

--- adminFrontend/theous_kai_frontend/src/AdminDashboard/AdminDashboard.jsx
+++ adminFrontend/theous_kai_frontend/src/AdminDashboard/AdminDashboard.jsx
@@ -6,11 +6,17 @@ const AdminDashboard = () => {
     <>
         <div className="container-fluid main-div p-0 m-0">
             <div className="row w-100 p-0 m-0 bg-primary">
-                <nav className='navbar'>
-                    <span class="navbar-brand mb-0 h1">
-                        <img src={icon} alt="Logo" width="40" height="40" className="my-2 mx-4 d-inline-block align-text-center"/>
+                <nav className='navbar navbar-expand'>
+                    <span className="navbar-brand mb-0 h1">
+                        <img src={icon} alt="Logo" width="45" height="45" className="p-1 border border-3 bg-secondary my-2 mx-4 d-inline-block align-text-center"/>
                         Theous Kai Dashboard
                     </span>
+                    <div className="container justify-content-start">
+                        <button className='btn btn-outline-warning btn-sm me-2 active'>Players</button>
+                    </div>
+                    <div className="container justify-content-end px-5">
+                        <button className='btn btn-danger'>Logout</button>
+                    </div>
                 </nav>
             </div>
         </div>
